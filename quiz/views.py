4283fff43b2246f1394d348e3af6975fbe446f9a
Darrel Nitereka <darrel.nitereka@wsu.edu>
2024-07-31 16:48:45 -0700

    Samples for adding question details and results

--- quiz/views.py
+++ quiz/views.py
@@ -8,6 +8,15 @@ def home(request):
 def quiz_detail(request, quiz_id):
     quiz = get_object_or_404(Quiz, id=quiz_id)
     questions = Question.objects.filter(quiz=quiz)
+    
+    if request.method == 'POST':
+        score = 0
+        for question in questions:
+            selected_option = request.POST.get(str(question.id))
+            if selected_option == question.correct_option:
+                score += 1
+        return render(request, 'quiz_result.html', {'quiz': quiz, 'score': score, 'total': len(questions)})
+
     return render(request, 'quiz_detail.html', {'quiz': quiz, 'questions': questions})
 
 def quiz_result(request, quiz_id):
