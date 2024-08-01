87a4eb40860a36661c069f47cdd1678b5b4c6d0d
Darrel Nitereka <darrel.nitereka@wsu.edu>
2024-07-31 17:44:19 -0700

    Modified Views

--- quiz/views.py
+++ quiz/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Quiz, Question
+from .models import Quiz, Question, Option
 
 def home(request):
     quizzes = Quiz.objects.all()
@@ -7,19 +7,23 @@ def home(request):
 
 def quiz_detail(request, quiz_id):
     quiz = get_object_or_404(Quiz, id=quiz_id)
-    questions = Question.objects.filter(quiz=quiz)
-    
+    questions = quiz.questions.all()
+
     if request.method == 'POST':
         score = 0
         for question in questions:
-            selected_option = request.POST.get(str(question.id))
-            if selected_option == question.correct_option:
-                score += 1
+            selected_options = request.POST.getlist(str(question.id))
+            correct_options = question.correct_options.all()
+            if question.question_type == Question.SINGLE_CHOICE:
+                if selected_options[0] in correct_options.values_list('id', flat=True):
+                    score += 1
+            elif question.question_type == Question.MULTIPLE_CHOICE:
+                if set(selected_options) == set(correct_options.values_list('id', flat=True)):
+                    score += 1
         return render(request, 'quiz_result.html', {'quiz': quiz, 'score': score, 'total': len(questions)})
 
     return render(request, 'quiz_detail.html', {'quiz': quiz, 'questions': questions})
 
 def quiz_result(request, quiz_id):
     quiz = get_object_or_404(Quiz, id=quiz_id)
-    # Logic to calculate score
     return render(request, 'quiz_result.html', {'quiz': quiz})
