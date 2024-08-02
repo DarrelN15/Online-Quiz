ae187fcbafdeefc95f84c50265e78d19bfe5d5b3
Darrel Nitereka <darrel.nitereka@wsu.edu>
2024-08-02 12:44:38 -0700

    Adding bootstrap styling

---
+++ quiz/templatetags/custom_filters.py
@@ -0,0 +1,8 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def num_to_letter(num):
+    # Convert number to ASCII letter, starting from 'A'
+    return chr(64 + num) if 1 <= num <= 26 else num
