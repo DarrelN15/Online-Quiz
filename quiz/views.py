f5795e62a290264947c2e433cb98bc060b84b3a4
Darrel Nitereka <darrel.nitereka@wsu.edu>
2024-08-01 17:46:50 -0700

    Sample Quiz testing True or False, and multiple answers working correctly.

--- quiz/views.py
+++ quiz/views.py
@@ -5,6 +5,7 @@ def home(request):
     quizzes = Quiz.objects.all()
     return render(request, 'home.html', {'quizzes': quizzes})
 
+
 def quiz_detail(request, quiz_id):
     quiz = get_object_or_404(Quiz, id=quiz_id)
     questions = quiz.questions.all()
@@ -12,18 +13,21 @@ def quiz_detail(request, quiz_id):
     if request.method == 'POST':
         score = 0
         for question in questions:
-            selected_options = request.POST.getlist(str(question.id))
+            selected_option_ids = request.POST.getlist(str(question.id))
+            selected_options = Option.objects.filter(id__in=selected_option_ids)
             correct_options = question.correct_options.all()
-            if question.question_type == Question.SINGLE_CHOICE:
-                if selected_options[0] in correct_options.values_list('id', flat=True):
-                    score += 1
-            elif question.question_type == Question.MULTIPLE_CHOICE:
-                if set(selected_options) == set(correct_options.values_list('id', flat=True)):
-                    score += 1
+
+            # Compare selected options with correct options
+            if set(selected_options) == set(correct_options):
+                score += 1
+
         return render(request, 'quiz_result.html', {'quiz': quiz, 'score': score, 'total': len(questions)})
 
     return render(request, 'quiz_detail.html', {'quiz': quiz, 'questions': questions})
 
+
 def quiz_result(request, quiz_id):
     quiz = get_object_or_404(Quiz, id=quiz_id)
     return render(request, 'quiz_result.html', {'quiz': quiz})
+
+
