588a85ee481e245a0b9a32b6b718039688c74b5e
Darrel Nitereka <darrel.nitereka@wsu.edu>
2024-08-09 10:25:22 -0700

    Fixing corrupted files

--- online_quiz_project/settings.py
+++ online_quiz_project/settings.py
@@ -126,3 +126,22 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+    },
+}
+
+LOGIN_REDIRECT_URL = '/'
--- quiz/models.py
+++ quiz/models.py
@@ -4,25 +4,27 @@ from django.contrib.auth.models import User
 class Quiz(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    # created_at = models.DateTimeField(auto_now_add=True)
+    # updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.title
 
 class Question(models.Model):
+    title = models.CharField(max_length=255, default="Default Question Title")
     quiz = models.ForeignKey(Quiz, related_name='questions', on_delete=models.CASCADE)
     text = models.CharField(max_length=200)
     QUESTION_TYPES = (
         ('SC', 'Single Choice'),
         ('MC', 'Multiple Choice'),
     )
-    question_type = models.CharField(max_length=2, choices=QUESTION_TYPES)
+    question_type = models.CharField(max_length=2, choices=QUESTION_TYPES, default='SC')
 
     def __str__(self):
         return self.text
 
 class Option(models.Model):
-    question = models.ForeignKey(Question, related_name='options', on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, related_name='options', on_delete=models.CASCADE, null=True, blank=True)
     text = models.CharField(max_length=200)
     is_correct = models.BooleanField(default=False)
 
@@ -32,7 +34,7 @@ class Option(models.Model):
 class QuizAttempt(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
-    score = models.IntegerField()
+    score = models.IntegerField(default=0)
     date_taken = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -41,9 +43,8 @@ class QuizAttempt(models.Model):
 class QuestionResponse(models.Model):
     attempt = models.ForeignKey(QuizAttempt, related_name='responses', on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    selected_options = models.ManyToManyField(Option)
+    selected_options = models.ManyToManyField(Option, blank=True)
     is_correct = models.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.question.text} - {self.is_correct}'
-
